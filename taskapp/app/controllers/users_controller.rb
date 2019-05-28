8885ffcf72a942c997e26153c33b52f284914b52
jkshareef <jamalkshareef@gmail.com>
2019-05-28 16:27:13 -0700

    Added basic login and signup functions

--- taskapp/app/controllers/users_controller.rb
+++ taskapp/app/controllers/users_controller.rb
@@ -1,2 +1,48 @@
 class UsersController < ApplicationController
+
+  before_action :set_user, only: [:edit, :show, :update]
+
+  def index
+
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def show
+
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in @user
+      flash[:success] = "Welcome to the Task App!"
+      redirect_to @user
+    else
+      render 'new'
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:display_name, :user_name, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
--- taskapp/config/routes.rb
+++ taskapp/config/routes.rb
@@ -1,9 +1,20 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   resources :requests
   resources :comments
   resources :suggestions
   resources :tasks
   resources :relationships
   resources :users
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
