c6e989c6aca64bc8e33ce5148e0e56b5aedff6b2
jkshareef <jamalkshareef@gmail.com>
2019-05-29 02:45:38 -0700

    Added Header and Friend Request Button

--- taskapp/app/controllers/requests_controller.rb
+++ taskapp/app/controllers/requests_controller.rb
@@ -1,10 +1,18 @@
 class RequestsController < ApplicationController
 
   def new
+    @request = Request.new
   end
+
+
 
   def create
     @user = User.find(params[:id])
-    Request.create_request(@user.id, current_user)
+
+    if Request.create_request(@user.id, current_user.id)
+      redirect_to current_user
+    else
+      redirect_to @user
+    end
   end
 end
