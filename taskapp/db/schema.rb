dfdfef8f94cd6163b2cd4b4925fa986dab17d457
jkshareef <jamalkshareef@gmail.com>
2019-05-31 00:21:45 -0700

    conditional for editing tasks implemented

--- taskapp/db/schema.rb
+++ taskapp/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_30_072717) do
+ActiveRecord::Schema.define(version: 2019_05_31_065239) do
 
   create_table "comments", force: :cascade do |t|
     t.string "content"
